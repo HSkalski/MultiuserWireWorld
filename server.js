7aba3a798f31b858222cf367962bbe5a50fe73a7
HSkalski <harrison.skalski@gmail.com>
2020-02-28 11:45:16 -0600

    further integration of board class

--- server.js
+++ server.js
@@ -13,32 +13,44 @@ var PORT = 2000;
 http.listen(PORT);
 console.log("Server started on port ", PORT);
 
+var SOCKET_LIST = {};
+var BOARD_LIST = {};
+
 var WIDTH = 1000;
 var HEIGHT = 600;
 var cellSize = 20;
 
+var createBoard = function(n,h,w,cs){
+    newBoard = new Board(n,h,w,cs);
+    BOARD_LIST[newBoard.id] = newBoard;
 
+    setInterval(function(){
+        BOARD_LIST[newBoard.id].update();
+    },1000/5)
 
-var defaultBoard = new Board('Default', HEIGHT, WIDTH, cellSize);
-defaultBoard.grid[10][10] = 1;
-defaultBoard.grid[10][11] = 1;
-defaultBoard.grid[10][12] = 2;
-defaultBoard.grid[10][13] = 3;
-defaultBoard.grid[9][14] = 1;
-defaultBoard.grid[9][9] = 1;
-defaultBoard.grid[8][10] = 1;
-defaultBoard.grid[8][11] = 1;
-defaultBoard.grid[8][12] = 1;
-defaultBoard.grid[8][13] = 1;
+    return newBoard.id;
+}
 
-var SOCKET_LIST = {};
-var BOARD_LIST = {};
-BOARD_LIST[defaultBoard.id] = defaultBoard;
+//var defaultBoard = new Board('Default', HEIGHT, WIDTH, cellSize);
+defaultBoardID = createBoard('Default', HEIGHT, WIDTH, cellSize);
+
+BOARD_LIST[defaultBoardID].grid[10][10] = 1;
+BOARD_LIST[defaultBoardID].grid[10][11] = 1;
+BOARD_LIST[defaultBoardID].grid[10][12] = 2;
+BOARD_LIST[defaultBoardID].grid[10][13] = 3;
+BOARD_LIST[defaultBoardID].grid[9][14] = 1;
+BOARD_LIST[defaultBoardID].grid[9][9] = 1;
+BOARD_LIST[defaultBoardID].grid[8][10] = 1;
+BOARD_LIST[defaultBoardID].grid[8][11] = 1;
+BOARD_LIST[defaultBoardID].grid[8][12] = 1;
+BOARD_LIST[defaultBoardID].grid[8][13] = 1;
+
+//BOARD_LIST[defaultBoard.id] = defaultBoard;
 
 io.on('connection', function (socket) {
     socket.id = Math.random();
     SOCKET_LIST[socket.id] = socket;
-    socket.boardID = defaultBoard.id;
+    socket.boardID = defaultBoardID;
     console.log('Connected Users: ', Object.keys(SOCKET_LIST).length);
 
     socket.emit('initData', {
@@ -86,25 +98,10 @@ setInterval(function () {
     for (var i in SOCKET_LIST) {
         var socket = SOCKET_LIST[i];
         socket.emit('boardData', {
-            board: BOARD_LIST[defaultBoard.id].grid
+            board: BOARD_LIST[defaultBoardID].grid
         })
         socket.emit('speedData', {
-            speed: (BOARD_LIST[defaultBoard.id].tickSpeed / BOARD_LIST[defaultBoard.id].tickReductionRatio)
+            speed: (BOARD_LIST[defaultBoardID].tickSpeed / BOARD_LIST[defaultBoardID].tickReductionRatio)
         })
     }
 }, 1000 / 24)
-
-// Update boards
-for(board_id in BOARD_LIST){
-    setInterval(function(){
-        BOARD_LIST[board_id].update();
-    },1000/5)
-}
-
-// var logicFunction = function () {
-//     board.logicInterval = setInterval(function () {
-//         board.update();
-//     }, 1000 / 5); //board.tickSpeed);
-// }
-
-// logicFunction();
